beb6a47d053936a45a4802efefb7c3b549323333
Elijah Paminsan <e.paminsan@gmail.com>
2019-06-06 14:48:57 -0700

    Changed para listener to copy and created listener for header image

--- js/index.js
+++ js/index.js
@@ -4,22 +4,26 @@
         2. mouseleave
         3. mousedown
         4. mouseup
-        5. dblclick
+        5. copy
+        6. dblclick
 */
 
 // hover over nav links
 const anchor = document.querySelectorAll('.nav-link');
-anchor.forEach((anchorOn) => { anchorOn.addEventListener('mouseover', () => { anchorOn.style.color = 'blue' })});
-anchor.forEach((anchorOff) => { anchorOff.addEventListener('mouseleave', () => { anchorOff.style.color = ''})}); 
+anchor.forEach((anchorOn) => { anchorOn.addEventListener('mouseover', () => { anchorOn.style.color = 'blue'; })});
+anchor.forEach((anchorOff) => { anchorOff.addEventListener('mouseleave', () => { anchorOff.style.color = ''; })}); 
 
 // click and release fun bus header logo
 const logoHead = document.querySelector('.logo-heading');
-logoHead.addEventListener('mousedown', () => { logoHead.innerHTML = "♪┏(・o･)┛♪" });
-logoHead.addEventListener('mouseup', () => { logoHead.innerHTML = "Fun Bus"})
+logoHead.addEventListener('mousedown', () => { logoHead.innerHTML = '♪┏(・o･)┛♪'; logoHead.style.color = 'orange'; });
+logoHead.addEventListener('mouseup', () => { logoHead.innerHTML = 'Fun Bus'; logoHead.style.color = ""; })
 
-// double click any of the text wrapped in p tags
-const theyStealing = () => { alert('Hey, no copying my content.') }
+// highlight and 'copy' and text wrapped in p tags
+const stealing = () => { alert('Hey, no copying my content.') }
 
 const para = document.querySelectorAll('p');
-para.forEach((function(elem){ elem.addEventListener('dblclick', theyStealing) }));
+para.forEach((function(elem){ elem.addEventListener('copy', stealing) }));
 
+// double click header photo (bus in the sand)
+const headImage = document.querySelector('.intro img');
+headImage.addEventListener('dblclick', () => { headImage.setAttribute('src', 'img/beach.png') })
