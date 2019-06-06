4b4acd3dd02399a9215ca20fa59aebfa817d48b4
Elijah Paminsan <e.paminsan@gmail.com>
2019-06-06 12:55:02 -0700

    Created two event listeners for nav anchors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+/* 
+    Events Used:
+        1. mouseover
+        2. mouseleave
+
+*/
+
+// hover for nav links
+let anchors = document.getElementsByClassName('nav-link')
+anchors[0].addEventListener('mouseover', () => { anchors[0].style.color= 'red'; });
+anchors[1].addEventListener('mouseover', () => { anchors[1].style.color= 'green'; });
+anchors[2].addEventListener('mouseover', () => { anchors[2].style.color= 'blue'; });
+anchors[3].addEventListener('mouseover', () => { anchors[3].style.color= 'pink'; });
+
+anchors[0].addEventListener('mouseleave', () => { anchors[0].style.color= ''; });
+anchors[1].addEventListener('mouseleave', () => { anchors[1].style.color= ''; });
+anchors[2].addEventListener('mouseleave', () => { anchors[2].style.color= ''; });
+anchors[3].addEventListener('mouseleave', () => { anchors[3].style.color= ''; });
